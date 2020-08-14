e3643af4ce188f748f1bdb3831a77d749a367dc3
Cecili <cecili.reid@gmail.com>
2020-08-14 14:32:05 -0400

    RECORDED 03_03 SUCCESSFULLY

--- src/test/java/com/cecilireid/puzzleinventory/PuzzleInventoryIntegrationTests.java
+++ src/test/java/com/cecilireid/puzzleinventory/PuzzleInventoryIntegrationTests.java
@@ -5,7 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,5 +31,19 @@ public class PuzzleInventoryIntegrationTests {
     public void testFindByName() throws Exception {
         Puzzle expected = new Puzzle();
         expected.setName("The Rialto Bridge");
+
+        mockMvc.perform(post("/puzzles").contentType(MediaType.APPLICATION_JSON_VALUE).content(mapper.writeValueAsString(expected))).andExpect(status().isOk());
+
+        Page<Puzzle> puzzles = puzzleRepository.findByName("The Rialto Bridge", null);
+
+        assertTrue(puzzles.getTotalElements() >= 1);
+
+        Optional<Puzzle> puzzle = puzzles.get().findFirst();
+        assertTrue(puzzle.isPresent());
+
+        Puzzle actual = puzzle.get();
+        assertEquals(expected.getName(), actual.getName());
+        assertNotNull(actual.getId());
+
     }
 }
