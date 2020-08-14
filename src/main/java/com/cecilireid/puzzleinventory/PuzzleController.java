18c82876b7204059f20e1a9f98c7270399b7375a
Cecili <cecili.reid@gmail.com>
2020-08-14 13:12:15 -0400

    Set up app for testing

--- src/main/java/com/cecilireid/puzzleinventory/PuzzleController.java
+++ src/main/java/com/cecilireid/puzzleinventory/PuzzleController.java
@@ -1,32 +1,63 @@
 package com.cecilireid.puzzleinventory;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("puzzles")
 public class PuzzleController {
-    private PuzzleRepository puzzleRepository;
-
-    public PuzzleController(PuzzleRepository puzzleRepository) {
-        this.puzzleRepository = puzzleRepository;
-    }
+    private final PuzzleRepository repository;
+    public PuzzleController(PuzzleRepository puzzleRepository) { this.repository = puzzleRepository;}
 
     @PostMapping
+    @ResponseBody
     public Puzzle create(@RequestBody Puzzle inputPuzzle) {
-        return puzzleRepository.save(inputPuzzle);
+        return repository.save(inputPuzzle);
     }
 
     @GetMapping("/{id}")
+    @ResponseBody
     public Puzzle read(@PathVariable Long id) {
-        return new Puzzle();
+        Optional<Puzzle> puzzle = repository.findById(id);
+        if (puzzle.isPresent()) {
+            return puzzle.get();
+        } else {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("/{id}")
+    @ResponseBody
     public Puzzle update(@RequestBody Puzzle inputPuzzle, @PathVariable Long id) {
-        return new Puzzle();
+        Optional<Puzzle> puzzle = repository.findById(id);
+        if (puzzle.isPresent()) {
+            // Hacky and not ideal
+            inputPuzzle.setId(id);
+            return repository.save(inputPuzzle);
+        }
+        else {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public Page<Puzzle> findAll(@RequestParam String name) {
+        return repository.findByName(name, Pageable.unpaged());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleClientException() {
     }
 }
--- src/main/java/com/cecilireid/puzzleinventory/PuzzleRepository.java
+++ src/main/java/com/cecilireid/puzzleinventory/PuzzleRepository.java
@@ -1,6 +1,16 @@
 package com.cecilireid.puzzleinventory;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PuzzleRepository extends CrudRepository<Puzzle, Long> {
+    /**
+     * Find Puzzles by its name
+     *
+     * @param name name of search to search for
+     * @param pageable pagination object to handled paged results
+     * @return Paged results of puzzles with matching name
+     */
+    Page<Puzzle> findByName(String name, Pageable pageable);
 }
