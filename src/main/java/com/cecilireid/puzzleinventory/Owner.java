4da6e2c672623133536f39a7d6e72b6472ad2073
Cecili <cecili.reid@gmail.com>
2020-08-14 12:16:35 -0400

    Setup challenge solution

---
+++ src/main/java/com/cecilireid/puzzleinventory/Owner.java
@@ -0,0 +1,53 @@
+package com.cecilireid.puzzleinventory;
+
+public class Owner {
+    private Long id;
+    private String name;
+    private String state;
+    private String country;
+    private String email;
+
+    public Owner() {
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
